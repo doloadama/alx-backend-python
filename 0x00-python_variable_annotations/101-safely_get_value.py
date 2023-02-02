d4f8c7e9d65b04207164ee8372d101e7d3f9926a
doloadama <doloadama@outlook.com>
2023-02-02 14:49:12 +0000

    12: Too long fixed

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,8 @@ from typing import Union, Any, Mapping, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                      default: Union[T, None] = None) -> Union[Any, T]:
     """
     Retrives a value from a dictionary using the given key
     """
