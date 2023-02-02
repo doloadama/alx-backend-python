97c57d73ef29c6206ec4b5c28bd8a7a18354ca37
doloadama <doloadama@outlook.com>
2023-02-02 09:01:09 +0000

    .

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Union, Tuple
+
+"""
+7. Complex types - string and int/float to tuple
+"""
+
+def to_kv(k: str, v: Union[int,float]) -> tuple[str, float]:
+  """
+  a type-annotated function to_kv that takes a string k and
+  an int OR float v as arguments and returns a tuple.
+  The first element of the tuple is the string k.
+  The second element is the square of the int/float v
+  and should be annotated as a float
+  """
+  return (k, float(v**2))
