dca73db6db275db73d2555273c3e9d664ce3aaa2
doloadama <doloadama@outlook.com>
2023-02-02 15:15:27 +0000

    12-Type_checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+12. Type Checking
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Creates multtiple copies of items in a tuple
+    """
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
