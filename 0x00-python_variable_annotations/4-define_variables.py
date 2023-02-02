704a13a5bc8f812e43e2854ae46f1db3b85225a0
doloadama <doloadama@outlook.com>
2023-02-02 10:03:33 +0000

    .

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -5,7 +5,7 @@ Define variables
 
 
 # define variables
-a : int = 1
-pi : float = 3.14
-i_understand_annotations : bool = True
-school : str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
