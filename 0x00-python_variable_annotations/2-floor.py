6dc9b6dbfd5d0c13daf9989e8f708ada8dbd35c3
doloadama <doloadama@outlook.com>
2023-02-02 08:23:55 +0000

    .

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+2. Basic annotations - floor
+"""
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float.
+    """
+    return int(n)
