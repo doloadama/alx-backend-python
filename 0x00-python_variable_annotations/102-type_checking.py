2ed747d4f9e6edc1061edfdbd304036db79d77e0
doloadama <doloadama@outlook.com>
2023-02-02 15:38:50 +0000

    .

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,7 +7,7 @@
 from typing import Tuple, List
 
 
-def zoom_array(lst: List, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Returns a new list with each element multiplied
     by factor.
@@ -18,7 +18,7 @@ def zoom_array(lst: List, factor: int = 2) -> List:
     ]
     return zoomed_in
 
-array = [12, 72, 91]
+array: Tuple = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
