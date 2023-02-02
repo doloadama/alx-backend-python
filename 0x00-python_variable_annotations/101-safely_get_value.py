b0bde87c0eae10c9696c8ecff65330815fc5958e
doloadama <doloadama@outlook.com>
2023-02-02 14:54:37 +0000

    indentations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,7 @@ T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-    default: Union[T, None] = None) -> Union[Any, T]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
     Retrives a value from a dictionary using the given key
     """
