0b680b3b1ca862f73f1bc5f909ca2ad8dcc63119
doloadama <doloadama@outlook.com>
2023-02-02 08:28:47 +0000

    .

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+3. Basic annotations - to string
+"""
+
+
+
+def to_str(n: float) -> str:
+  """
+  Convert a number to a string.
+  """
+  return str(n)
