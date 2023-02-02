a65016e3af697a04a0dedf996a924cab91f3e5da
doloadama <doloadama@outlook.com>
2023-02-02 09:29:57 +0000

    .

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,8 +2,9 @@
 """
 0. Basic annotations - add
 """
+
 # Typed annotations
 def add(a: float, b: float) -> float:
-  """Concatenates two floats
-  """
-  return a + b
+    """Concatenates two floats
+    """
+    return a + b
