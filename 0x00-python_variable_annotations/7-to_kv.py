b878482ffe406bdc1eee50596960cad3f5ca45e5
doloadama <doloadama@outlook.com>
2023-02-02 10:17:43 +0000

    .

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,7 +8,7 @@ from typing import Union, Tuple
 """
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     a type-annotated function to_kv that takes a string k and
     an int OR float v as arguments and returns a tuple.
