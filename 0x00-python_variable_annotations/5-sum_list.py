9bd773107970039ab9a26824bd7c6de03a43aa63
doloadama <doloadama@outlook.com>
2023-02-02 10:14:23 +0000

    .

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,7 +7,7 @@
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Returns the sum of all the elements in the list
     """
