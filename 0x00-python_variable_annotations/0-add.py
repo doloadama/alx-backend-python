8e0650341f23cba506eed057f91c02e2f88b0c68
doloadama <doloadama@outlook.com>
2023-02-02 09:11:07 +0000

    .

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,8 +4,6 @@
 """
 
 def add(a: float, b: float) -> float:
-  """
-  a type-annotated function add that takes a float a and a
-  float b as arguments and returns their sum as a float.
+  """Concatenates two floats
   """
   return a + b
