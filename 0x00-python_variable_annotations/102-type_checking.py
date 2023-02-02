d5935f1c1a444ddc6aa17d83045c223fe857f805
doloadama <doloadama@outlook.com>
2023-02-02 15:33:38 +0000

    .

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,7 +6,7 @@
 from typing import Tuple, List
 
 
-def zoom_array(lst: List, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
