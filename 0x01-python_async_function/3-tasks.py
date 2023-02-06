4a32a9613caca57074f9d0817537cd12b4ee8d28
doloadama <doloadama@outlook.com>
2023-02-06 23:42:14 +0000

    .

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,7 +6,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """_summary_
 
     Args:
