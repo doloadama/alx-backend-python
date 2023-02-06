d6eae81ed865bb587b0bf646e2778caa54fd03f0
doloadama <doloadama@outlook.com>
2023-02-06 23:30:37 +0000

    .

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """_summary_
     Args:
         n (_type_): _description_
