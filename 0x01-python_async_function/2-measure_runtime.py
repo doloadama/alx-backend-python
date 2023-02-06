2fddcae83d21aeee0b620fddecf9fff2fafd974d
doloadama <doloadama@outlook.com>
2023-02-06 21:45:57 +0000

    .

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,23 +2,20 @@
 """
 2. Measure the runtime
 """
-
-
-import time
+from time import perf_counter
 wait_n = __import__('1-concurrent_coroutines').wait_n
+import asyncio
 
 
 async def measure_time(n: int, max_delay: int) -> float:
     """_summary_
-
     Args:
         n (_type_): _description_
         max_delay (_type_): _description_
     Returns:
         _type_: _description_
     """
-    start = time.time()
-    wait_n(n, max_delay)
-    end = time.time()
-    total_time = end - start
-    return total_time / n
+    start = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elapsed = perf_counter() - start
+    return elapsed / n
