c375bfd39d21820f316bd42b2536d5275f4c39ff
doloadama <doloadama@outlook.com>
2023-02-06 23:45:44 +0000

    .

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """_summary_
 
     Args:
