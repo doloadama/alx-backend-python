a040310c3801fda674ab7705eae52c91e465f689
doloadama <doloadama@outlook.com>
2023-02-06 23:10:32 +0000

    .

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,8 +3,8 @@
 1. Let's execute multiple coroutines at the same time with async
 """
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
