064221c0c580e0edc775db122c1971973c5278e2
doloadama <doloadama@outlook.com>
2023-02-07 12:54:07 +0000

    .

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """
     collect 10 random numbers using an async comprehension over async_generator
     then return 10 random numbers
