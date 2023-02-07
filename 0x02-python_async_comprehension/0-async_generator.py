614239321234bfbd4b5d38860d6a433941a383e2
doloadama <doloadama@outlook.com>
2023-02-07 07:19:51 +0000

    return value corretcted

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,9 @@
 """
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """loop 10 times, each time asynchronously
     wait 1 second, then yield a random number
     between 0 and 10
