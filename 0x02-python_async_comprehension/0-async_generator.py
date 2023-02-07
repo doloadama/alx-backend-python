f3e884f9f48336f4b0834ac55b8407ab5f4da147
doloadama <doloadama@outlook.com>
2023-02-07 07:22:00 +0000

    random

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,4 +14,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint() * 10
+        yield random.random() * 10
