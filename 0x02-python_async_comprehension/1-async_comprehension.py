7a6137f91aaa0d97fc71409040c854e4f6504fae
doloadama <doloadama@outlook.com>
2023-02-07 09:57:44 +0000

    .

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """1-Async Comprehension
 """
-import asyncio
+from importlib import import_module as using
 from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
@@ -11,6 +11,4 @@ async def async_comprehension() -> Generator[float, None, None]:
     collect 10 random numbers using an async comprehension over async_generator
     then return 10 random numbers
     """
-
-    async for i in async_generator(10):
-        print(i)
+    return [num async for num in async_generator()]
