f47126096d52bd2f5c8fed3a63d49c22cf42d103
doloadama <doloadama@outlook.com>
2023-02-07 12:15:13 +0000

    output issue fixed

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """1-Async Comprehension
 """
-from importlib import import_module as using
 from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
@@ -11,4 +10,5 @@ async def async_comprehension() -> Generator[float, None, None]:
     collect 10 random numbers using an async comprehension over async_generator
     then return 10 random numbers
     """
-    return [num async for num in async_generator()]
+    rslt = [i async for i in async_generator()]
+    return rslt
