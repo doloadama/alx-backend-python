1fd15af22cd8e77039775ed66c2ab37f6c62df96
doloadama <doloadama@outlook.com>
2023-02-26 21:19:15 +0000

    .

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -80,16 +80,14 @@ class TestMemoize(unittest.TestCase):
         class TestClass:
             def a_method(self) -> None:
                 return 42
-            
-
             @memoize
             def a_property(self):
                 return self.a_method()
         with patch.object(
-            TestClass,
-            "a_method",
-            return_value=lambda: 42,
-            ) as memo_fxn:
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
