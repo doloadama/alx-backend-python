d7175a57be6bd742b7c8c898a4f57922a09eb1c7
doloadama <doloadama@outlook.com>
2023-02-27 17:03:54 +0000

    import file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,6 +7,7 @@ from unittest.mock import patch, PropertyMock
 from parameterized import parameterized, parameterized_class
 import client
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
